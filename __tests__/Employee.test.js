8994fbc976acae2d188f08714995a1d9af0f39ce
Kurt Bixby <kurtbixby@mac.com>
2022-07-22 20:57:26 -0500

    Started writing tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,66 @@
+const fs = require('fs/promises');
+
+const Employee = require('../lib/Employee');
+
+jest.mock('fs/promises');
+
+describe('Employee', () => {
+    describe('constructor', () => {
+        it('should throw an error', () => {
+            expect(() => new Employee()).toThrow('missing Employee constructor parameter');
+        });
+
+        it('should construct successfully', () => {
+            const [ name, id, email ] = [ 'Sam', 4, 'sam@job.com' ];
+            const employee = new Employee(name, id, email);
+
+            expect(employee.name).toEqual(name);
+            expect(employee.id).toEqual(id);
+            expect(employee.email).toEqual(email);
+            expect(employee.role).toEqual('Employee');
+        })
+    });
+
+    describe('getters', () => {
+        const [ name, id, email ] = [ 'Sam', 4, 'sam@job.com' ];
+        const employee = new Employee(name, id, email);
+
+        it('getName()', () => {
+            expect(employee.getName()).toEqual(name);
+        });
+
+        it('getId()', () => {
+            expect(employee.getId()).toEqual(id);
+        });
+
+        it('getEmail()', () => {
+            expect(employee.getEmail()).toEqual(email);
+        });
+
+        it('getRole()', () => {
+            expect(employee.getRole()).toEqual('Employee');
+        });
+    });
+
+    describe('generateHtml', () => {
+        it('should fill in the template fields', async () => {
+            const testValues = {name:'Sam', role:'Employee'};
+            fs.readFile = jest.fn().mockResolvedValue('Name:${values.name} Role:${values.role}');
+
+            const filledTemplate = await Employee._generateHtml('../src/employeeTemplate.html', testValues);
+
+            expect(filledTemplate).toEqual('Name:Sam Role:Employee')
+        });
+    });
+
+    describe('createHtmlCard', () => {
+        it('should fill in the template fields', async () => {
+            const sam = new Employee('Sam', 4, 'sam@job.com');
+            fs.readFile = jest.fn().mockResolvedValue('Name:${values.name} Role:${values.role} Id:${values.id} Email:${values.email}');
+
+            const filledTemplate = await sam.createHtmlCard();
+
+            expect(filledTemplate).toEqual('Name:Sam Role:Employee Id:4 Email:sam@job.com')
+        });
+    })
+});
